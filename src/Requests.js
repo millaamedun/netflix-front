c8dbe46195cb0b1001ad6080619b0d6a4d934ab4
millaamedun <133498951+millaamedun@users.noreply.github.com>
2023-08-18 19:43:32 +0330

    Update Requests.js

--- src/Requests.js
+++ src/Requests.js
@@ -1,11 +1,10 @@
-const key = process.env.REACT_APP_IMDB_API_KEY
 
 const requests = {
-    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
-    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
-    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
-    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=9d3d1c2f3990d746d8b0a92004fe2231&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=9d3d1c2f3990d746d8b0a92004fe2231&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=9d3d1c2f3990d746d8b0a92004fe2231&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=9d3d1c2f3990d746d8b0a92004fe2231&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=9d3d1c2f3990d746d8b0a92004fe2231&language=en-US&page=1`,
   };
 
   export default requests
